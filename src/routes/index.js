cf5fe59a1932d5920f4d6ac1ef8cd8ca267565ab
wangdahoo <157195705@qq.com>
2016-06-30 10:48:03 +0800

    zen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,18 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout/CoreLayout'
 import Home from './Home'
-import CounterRoute from './Counter'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
-export const createRoutes = (store) => ({
-  path: '/',
-  component: CoreLayout,
-  indexRoute: Home,
-  childRoutes: [
-    CounterRoute(store)
-  ]
-})
+//export const createRoutes = (store) => ({
+//  path: '/',
+//  component: CoreLayout,
+//  indexRoute: Home,
+//  childRoutes: [
+//    CounterRoute(store)
+//  ]
+//})
 
 /*  Note: childRoutes can be chunked or otherwise loaded programmatically
     using getChildRoutes with the following signature:
@@ -32,5 +31,26 @@ export const createRoutes = (store) => ({
     inside the route `getComponent` function, since it is only invoked
     when the route exists and matches.
 */
+
+export const createRoutes = (store) => {
+
+  const routes = {
+    path: '/',
+    component: CoreLayout,
+    indexRoute: Home,
+    getChildRoutes (location, next) {
+      require.ensure([], (require) => {
+        next(null, [
+          // Provide store for async reducers and middleware
+          require('./Counter').default(store),
+          require('./Zen').default(store),
+          require('./NotFound').default
+        ])
+      })
+    }
+  }
+
+  return routes
+}
 
 export default createRoutes
